25f5fab6f3dc7837b65ab3b8b2c79fd4d0b27081
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2019-03-05 11:52:36 +0000

    Update RemoveJava.ps1

--- RemoveJava.ps1
+++ RemoveJava.ps1
@@ -6,44 +6,48 @@ $JavaPackages = Get-WmiObject -Class Win32_InstalledWin32Program | Where-Object 
 
 #Get current Java program
 $CurrentJavaPackage = Get-WmiObject -Class Win32_InstalledWin32Program | Where-Object { $_.Name -eq 'Java 8 Update 201' } | Select Name, MsiProductCode, Version
+
 $RegistryPathCurrentJava = Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$($CurrentJavaPackage.MsiProductCode)" -Name "InstallLocation"
 
 #Initialise array for Install Locations for each Java program
 $Locations = @()
 
-#Remove Java Programs
-ForEach ($JavaProgram in $JavaPackages)
+If (!$JavaPackages)
 {
-	#Get InstallLocation from Registry
-	$RegistryPath = Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$($JavaProgram.MsiProductCode)" -Name "InstallLocation"
-	
-	#Create New Object for Java Location
-	$Path = New-Object PSObject
-	$Path | Add-Member -Type NoteProperty -Name 'InstallLocation' -value $RegistryPath.InstallLocation
-	$Locations += $Path
-	
-	#Uninstall Java
-	Write-Host "Uninstalling:" $($JavaProgram.Name)
-	Write-Host "Location:" $Path.InstallLocation
-	Start-Process msiexec.exe "/x $($JavaProgram.MsiProductCode) /qn" -Wait
+	Write-Host "No qualifying Java Programs to Uninstall"
 }
-
-#Cleanup old Java Folders
-ForEach ($Location in $Locations)
+else
 {
-	#CleanUp Java Source Folder after MSIs removed
-	Write-Host "Checking if """$($Location.InstallLocation)""" needs cleaning up"
-	If (Test-Path -Path $Location.InstallLocation)
+	#Remove Java Programs
+	ForEach ($JavaProgram in $JavaPackages)
 	{
-		Write-Host "Found left over folder:"$Location.InstallLocation
-		If ($Location.InstallLocation -ne $CurrentJavaPackage.InstallLocation)
+		#Get InstallLocation from Registry
+		$RegistryPath = Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$($JavaProgram.MsiProductCode)" -Name "InstallLocation"
+		
+		#Create New Object for Java Location
+		$Path = New-Object PSObject
+		$Path | Add-Member -Type NoteProperty -Name 'InstallLocation' -value $RegistryPath.InstallLocation
+		$Locations += $Path
+		
+		#Uninstall Java
+		Write-Host "Uninstalling:" $($JavaProgram.Name)
+		Start-Process msiexec.exe "/x $($JavaProgram.MsiProductCode) /qn" -Wait
+	}
+	
+	#Cleanup old Java Folders
+	ForEach ($Location in $Locations)
+	{
+		#CleanUp Java Source Folder after MSIs removed
+		Write-Host "Checking if """$($Location.InstallLocation)""" needs cleaning up"
+		If (Test-Path -Path $Location.InstallLocation)
 		{
-			Remove-Item -LiteralPath $Location.InstallLocation -Force -Recurse
-			Write-Host "Cleaning up Directory:" $Location.InstallLocation
-		}
-		else
-		{
-			Write-Host "Directory Cleanup not required for:"$Location.InstallLocation
+			Write-Host "Found left over folder:"$Location.InstallLocation
+			If ($Location.InstallLocation -ne $CurrentJavaPackage.InstallLocation)
+			{
+				Remove-Item -LiteralPath $Location.InstallLocation -Force -Recurse
+				Write-Host "Cleaning up Directory:" $Location.InstallLocation
+			}
 		}
 	}
-}	
+		Write-Host "Directory Cleanup Complete"
+}

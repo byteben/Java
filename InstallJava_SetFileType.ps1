0ac3a6c3d0f54ad5bb4d4c791d7c7b1a6f0cad3d
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2019-03-07 07:59:07 +0000

    Update InstallJava_SetFileType.ps1

--- InstallJava_SetFileType.ps1
+++ InstallJava_SetFileType.ps1
@@ -1,20 +1,10 @@
-#Set Current Directory Script launched from
-$ScriptRoot = Split-Path -Path $MyInvocation.MyCommand.Path
-
 #Specify Java Installer
-$JavaProg = $ScriptRoot+'\jre-7u15-windows-i586.exe'
-
-#Specify Installer Arguments
-$JavaArgs = '/s INSTALL_SILENT=Enable SPONSORS=0 AUTO_UPDATE=Disable REBOOT=Disable REMOVEOLDERJRES=0 REMOVEOUTOFDATEJRES=0'
-
+$JavaProg = 'jre1.7.0_15.msi'
 #Install Java
-Start-Process $JavaProg $JavaArgs -NoNewWindow -Wait -PassThru
-
+Start-Process msiexec.exe "/qn  /i $($JavaProg) INSTALL_SILENT=Enable SPONSORS=0 AUTO_UPDATE=Disable REBOOT=Disable REMOVEOLDERJRES=0 REMOVEOUTOFDATEJRES=0" -Wait
 #Wait a further 5 seconds (In testing, the file association didnt work if ran immediatly after Java installtion)
 Start-Sleep -s 5
-
 Write-Host "Setting .jnlp file association to Java 7 U 15 javaws.exe"
-
 #Associate .jnlp files with javaws.exe
 $fileType = (cmd /c "assoc .jnlp")
 $fileType = $fileType.Split("=")[-1]
